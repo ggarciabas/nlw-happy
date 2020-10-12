15023d06693ddff81e0d0ac925494a7a378b13f5
Giovanna <ggarciabas@gmail.com>
2020-10-12 18:52:41 -0300

    feat: conteúdo da página principal

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,11 +2,25 @@ import React from 'react';
 import './styles/global.css';
 import './styles/pages/landing.css';
 
+import logoImg from './images/logo.svg';
 
 function App() {
   return (
     <div id='page-landing' >
-      <h1>Hello World</h1>
+      <div className="content-wrapper">
+        <img src={logoImg} alt='happy'/>
+        <main>
+          <h1>Leve a felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+        <div className="location">
+          <strong>Curitiba</strong>
+          <span>Paraná</span>
+        </div>
+        <a href="" className=''>
+          
+        </a>
+      </div>
     </div>
   );
 }
