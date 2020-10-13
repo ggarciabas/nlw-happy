78a93052010f1c9824c1200a618f1d627a3191b9
Giovanna <ggarciabas@gmail.com>
2020-10-13 18:54:35 -0300

    feat: conteúdo pagina do mapa

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {FiPlus} from 'react-icons/fi';
+import mapMarkerImg from '../images/map-pin.svg';
 
 function OrphanagesMap () {
     return (
-        <h1>Hello World</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt=""/>
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+                <footer>
+                    <strong>Curitiba</strong>
+                    <span>Paraná</span>
+                </footer>
+            </aside>
+
+            <div className="">
+                {/* mapa */}
+            </div>
+
+            <Link to='' className='create-orphanage'>
+                <FiPlus size={12} color='#FFF' />
+            </Link>
+        </div>
     );
 }
 
