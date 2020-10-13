4a736383261ca4ad207f0c62a562e60061bf4461
ggarciabas <ggarciabas@users.noreply.github.com>
2020-10-13 22:21:41 +0000

    deploy: 4a63fe3a044eefac9b963fabb7f7a205d10332fc

---
+++ precache-manifest.141f4260ef9b8532e4c054f056b9ce8c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47838ac90b2a5a7b8d2f0431d8fd2f98",
+    "url": "/nlw-happy/index.html"
+  },
+  {
+    "revision": "ee27ca9d79a04f7a74db",
+    "url": "/nlw-happy/static/css/main.2f0caec5.chunk.css"
+  },
+  {
+    "revision": "669b6f65d9d37b6cdd92",
+    "url": "/nlw-happy/static/js/2.7ae90a0a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nlw-happy/static/js/2.7ae90a0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee27ca9d79a04f7a74db",
+    "url": "/nlw-happy/static/js/main.347a81e5.chunk.js"
+  },
+  {
+    "revision": "f5ca312b900a7fc0a953",
+    "url": "/nlw-happy/static/js/runtime-main.fd5cfd42.js"
+  },
+  {
+    "revision": "91c8d1af04a8fbdde51d2b16c5471b15",
+    "url": "/nlw-happy/static/media/landing.91c8d1af.svg"
+  },
+  {
+    "revision": "1653c64706c95e2e85a07cc755abf8ba",
+    "url": "/nlw-happy/static/media/logo-text.1653c647.svg"
+  },
+  {
+    "revision": "284d1ea859a0eb7d0ed785dbcc775752",
+    "url": "/nlw-happy/static/media/map-pin.284d1ea8.svg"
+  }
+]);
