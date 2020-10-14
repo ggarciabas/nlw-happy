b42938f041fa87896815a0eb92d490a2da4c77d6
Giovanna <ggarciabas@gmail.com>
2020-10-13 22:38:54 -0300

    feat: rotas

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,10 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json()) /* para que o express entenda o JSON default */
+
+app.get('/users', (request, response) => {
+    return response.send({'message':'Hello'});
+})
 
 app.listen(3333);
