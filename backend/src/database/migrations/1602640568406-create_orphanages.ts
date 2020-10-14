fd7334cf0e0146fd35296185dad6611886127a53
Giovanna <ggarciabas@gmail.com>
2020-10-13 23:12:30 -0300

    fix: adiciona campo

--- backend/src/database/migrations/1602640568406-create_orphanages.ts
+++ backend/src/database/migrations/1602640568406-create_orphanages.ts
@@ -1,11 +1,59 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { query } from "express";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class createOrphanages1602640568406 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: "orphanages",
+      columns: [
+        {
+          name: "id",
+          type: "integer",
+          unsigned: true,
+          isPrimary: true,
+          isGenerated: true,
+          generationStrategy: "increment",
+        },
+        {
+          name: "name",
+          type: "varchar"
+        },
+        {
+          name: "latitude",
+          type: "decimal",
+          scale: 10,
+          precision: 2,
+        },
+        {
+          name: "longitude",
+          type: "decimal",
+          scale: 10,
+          precision: 2,
+        },
+        {
+          name: "about",
+          type: "text",
+        },
+        {
+          name: "instructions",
+          type: "text",
+        },
+        {
+          name: "opening_hours",
+          type: "varchar"
+        },
+        {
+          name: "open_on_wekends",
+          type: "boolean",
+          default: false,
+        }
+      ]
+    }))
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orphanages');
+  }
 
 }
