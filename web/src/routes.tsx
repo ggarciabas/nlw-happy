5c479f1c842683b3d03f0a092e15415369fed34c
Giovanna <ggarciabas@gmail.com>
2020-10-17 17:22:29 -0300

    feat: novas rotas

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap'
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
 
 function Routes () {
     return (
@@ -9,6 +11,8 @@ function Routes () {
             <Switch>
                 <Route path='/nlw-happy' exact component={Landing} />
                 <Route path='/nlw-happy/app' component={OrphanagesMap} />
+                <Route path='/nlw-happy/orphanage/create' component={Orphanage} />
+                <Route path='/nlw-happy/orphanage/:id' component={CreateOrphanage} />
             </Switch>
         </BrowserRouter>
     );
