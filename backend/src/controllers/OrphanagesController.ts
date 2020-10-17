4e573a59c837955376e5b9087ae811cdb79d805d
Giovanna <ggarciabas@gmail.com>
2020-10-17 15:14:35 -0300

    feat: adiciona validacao de campos com Yup

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -2,6 +2,7 @@ import {Request, Response} from 'express';
 import {getRepository} from 'typeorm';
 import Orphanege from '../models/Orphanage';
 import OrphanagesView from '../views/OrphanagesView';
+import * as Yup from 'yup';
 
 export default {
     async index (request: Request, response: Response) {
@@ -35,7 +36,28 @@ export default {
             return {path: image.filename }
         }) // percorre cada imagem retornando o caminho
 
-        const orphanage = orphanagesRepository.create({name, latitude, longitude, about, instructions, opening_hours, open_on_weekends, images});
+        const data = {name, latitude, longitude, about, instructions, opening_hours, open_on_weekends, images} 
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array(
+                Yup.object().shape({
+                    path: Yup.string().required()
+                })
+            )
+        });
+
+        await schema.validate(data, {
+            abortEarly: false // para retornar todos os erros e não somente o primeiro
+        });
+
+        const orphanage = orphanagesRepository.create(data);
 
         await orphanagesRepository.save(orphanage); // salva no banco
 
