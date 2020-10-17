8e766eda2a46af9f52b66401c5226192a58f72d1
Giovanna <ggarciabas@gmail.com>
2020-10-17 13:37:33 -0300

    feat: cria controller

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,21 +1,10 @@
 import express from 'express';
 import './database/connection';
-import {getRepository} from 'typeorm';
-import Orphanege from './models/Orphanage';
+import OrphanagesController from './controllers/OrphanagesController';
 
 const app = express();
 app.use(express.json()) /* para que o express entenda o JSON default */
 
-app.post('/orphanages', async (request, response) => {
-    const {name, latitude, longitude, about, instructions, opening_hours, open_on_weekends} = request.body;
-
-    const orphanagesRepository = getRepository(Orphanege);
-
-    const orphanage = orphanagesRepository.create({name, latitude, longitude, about, instructions, opening_hours, open_on_weekends});
-
-    await orphanagesRepository.save(orphanage); // salva no banco
-
-    return response.status(201).json(orphanage); // 201 - algo foi criado
-})
+app.post('/orphanages', OrphanagesController.create)
 
 app.listen(3333);
