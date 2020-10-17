63c22cc9a9d49df942be8de74c8cb8793e3b580b
Giovanna <ggarciabas@gmail.com>
2020-10-17 15:04:33 -0300

    feat: publica uma pasta usando express

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import './database/connection';
 import routes from './routes';
+import path from 'path';
 
 const app = express();
 
 app.use(express.json()) /* para que o express entenda o JSON default */
 app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads'))) // para publicar uma pasta de forma estática 
 
 app.listen(3333);
