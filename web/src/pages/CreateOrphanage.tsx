1776d4cfce919d67f0fdecf7d8e30092724e8b79
Giovanna <ggarciabas@gmail.com>
2020-10-18 08:21:05 -0300

    feat: atualiza css para recurso de fotos

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -84,13 +84,11 @@ export default function CreateOrphanage() {
             <div className="input-block">
               <label htmlFor="images">Fotos</label>
 
-              <div className="uploaded-image">
-
+              <div className="images-container">
+                <button className="new-image" type='button'>
+                  <FiPlus size={24} color="#15b6d6" />
+                </button>
               </div>
-
-              <button className="new-image">
-                <FiPlus size={24} color="#15b6d6" />
-              </button>
             </div>
           </fieldset>
 
